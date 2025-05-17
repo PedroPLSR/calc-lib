6f96811071e07e0ea67808d05b2b80a432f55f75
gabriel-fonseca-repo <gabrielfonsecafaf@gmail.com>
2025-05-17 16:37:54 -0300

    Criando classe para elaboração de testes

---
+++ src/test/java/org/fbp/CalculatorTest.java
@@ -0,0 +1,9 @@
+package org.fbp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {}
